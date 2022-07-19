a2b42d4817e03516108230b1e4c999a33026dbd8
呼呼北风 <2535357585@qq.com>
2022-07-20 00:28:50 +0800

    opt

--- src/main/java/leetcode/top100/MyCalendarTwo.java
+++ src/main/java/leetcode/top100/MyCalendarTwo.java
@@ -1,7 +1,6 @@
 package leetcode.top100;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author HuWei05
@@ -11,24 +10,43 @@ public class MyCalendarTwo {
     List<int[]> booked;
     List<int[]> overlaps;
 
+//    public MyCalendarTwo() {
+//        booked = new ArrayList<>();
+//        overlaps = new ArrayList<>();
+//    }
+//
+//    public boolean book(int start, int end) {
+//        for (int[] arr :overlaps ) {
+//            if (arr[0]<start && arr[1]<end){
+//                return false;
+//            }
+//        }
+//        for (int[] arr:booked ) {
+//            if (arr[0]<start && arr[1]<end){
+//                overlaps.add(new int[]{Math.max(arr[0],start),Math.min(end,arr[1])});
+//            }
+//        }
+//        booked.add(new int[]{start,end});
+//        return true;
+//
+//    }
+TreeMap<Integer, Integer> map;
     public MyCalendarTwo() {
-        booked = new ArrayList<>();
-        overlaps = new ArrayList<>();
+        map = new TreeMap<>();
     }
 
     public boolean book(int start, int end) {
-        for (int[] arr :overlaps ) {
-            if (arr[0]<start && arr[1]<end){
+        int cnt=0;
+        map.put(start, map.getOrDefault(start, 0) + 1);
+        map.put(end, map.getOrDefault(end, 0) - 1);
+        for (Map.Entry<Integer, Integer>  entry:map.entrySet()){
+            cnt+=entry.getValue();
+            if (cnt>2){
+                map.put(start, map.getOrDefault(start, 0) - 1);
+                map.put(end, map.getOrDefault(end, 0) + 1);
                 return false;
             }
         }
-        for (int[] arr:booked ) {
-            if (arr[0]<start && arr[1]<end){
-                overlaps.add(new int[]{Math.max(arr[0],start),Math.min(end,arr[1])});
-            }
-        }
-        booked.add(new int[]{start,end});
         return true;
-
     }
 }
