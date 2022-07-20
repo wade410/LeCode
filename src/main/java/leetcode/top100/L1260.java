676c677265b89ef8479d8081d04f0eb279d6cd07
呼呼北风 <2535357585@qq.com>
2022-07-21 00:50:11 +0800

    L1206

---
+++ src/main/java/leetcode/top100/L1260.java
@@ -0,0 +1,30 @@
+package leetcode.top100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author HuWei05
+ * @Date 2022/7/21
+ **/
+public class L1260 {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        List<List<Integer>> lists = new ArrayList<>();
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            List<Integer> subList = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                subList.add(0);
+            }
+            lists.add(subList);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int newIndex  = (i*n+j+k)%(m*n);
+                lists.get(newIndex/n).set(newIndex%n,grid[i][j]);
+            }
+        }
+        return lists;
+    }
+}
