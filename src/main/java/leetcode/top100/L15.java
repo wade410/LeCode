85693853a7cc0bc9ea13c4595d1704f6eb9a063a
呼呼北风 <2535357585@qq.com>
2022-07-24 10:44:58 +0800

    L15

---
+++ src/main/java/leetcode/top100/L15.java
@@ -0,0 +1,59 @@
+package leetcode.top100;
+
+import java.util.*;
+
+/**
+ * @Author HuWei05
+ * @Date 2022/7/24
+ **/
+public class L15 {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        if(nums==null || nums.length<3){
+            return null;
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < n - 2; i++) {
+            if (i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int temp = nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            while (left<right){
+                int sum = nums[left]+nums[right]+temp;
+                if (sum==0){
+                    res.add(Arrays.asList(temp, nums[left], nums[right]));
+                    while (left + 1 < n && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    left++;
+                    while (right-1>0 && nums[right] == nums[right - 1]){
+                        right--;
+                    }
+                    right--;
+
+                }else if (sum<0){
+                    while (left + 1 < n && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    left++;
+                }else {
+                    while (right-1>0 && nums[right] == nums[right - 1]){
+                        right--;
+                    }
+                    right--;
+                }
+            }
+
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        System.out.println(threeSum(nums));
+    }
+}
---
+++ src/main/java/leetcode/top100/O115.java
@@ -0,0 +1,34 @@
+package leetcode.top100;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @Author HuWei05
+ * @Date 2022/7/23
+ **/
+public class O115 {
+    public static boolean sequenceReconstruction(int[] nums, int[][] sequences) {
+        Map<Integer, Set> d = new HashMap<>();
+        for (int[] x : sequences) {
+            for (int i = 1; i < x.length; i++) {
+                d.putIfAbsent(x[i - 1], new HashSet<Integer>());
+                d.get(x[i - 1]).add(x[i]);
+            }
+        }
+        for (int i = 1; i < nums.length; i++) {
+            if (!d.getOrDefault(nums[i - 1], new HashSet<Integer>()).contains(nums[i]))
+                return false;
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        int[][] sequences = {{1,2},{1,3},{2,3}};
+        System.out.println(sequenceReconstruction(nums,sequences));
+    }
+}
