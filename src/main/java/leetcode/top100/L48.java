4f96bfa77d46fd1fadd88dd4017294f8af2ad6c8
呼呼北风 <2535357585@qq.com>
2022-08-10 23:53:24 +0800

    L

---
+++ src/main/java/leetcode/top100/L48.java
@@ -0,0 +1,29 @@
+package leetcode.top100;
+
+/**
+ * @Author HuWei05
+ * @Date 2022/8/10
+ **/
+public class L48 {
+    ///这种题比较有意思，也比较难想
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+
+    }
+
+}
