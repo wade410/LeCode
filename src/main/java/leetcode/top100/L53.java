c3038573fcb885aae00731f7a35e9d201ec0fa70
呼呼北风 <2535357585@qq.com>
2022-08-12 23:44:33 +0800

    L53

--- src/main/java/leetcode/top100/L53.java
+++ src/main/java/leetcode/top100/L53.java
@@ -6,7 +6,18 @@ package leetcode.top100;
  **/
 public class L53 {
     public int maxSubArray(int[] nums) {
-
-
+        if (nums==null)
+            return 0;
+        int max=nums[0];
+        int cur =nums[0];
+        for (int i = 1; i < nums.length ; i++) {
+            if (cur<0){
+                cur=nums[i];
+            }else {
+                cur=cur+nums[i];
+            }
+            max=Math.max(max,cur);
+        }
+        return max;
     }
 }
