001d31a72f39945fa83c274fefeb46b68fe394ce
呼呼北风 <2535357585@qq.com>
2022-08-14 23:02:24 +0800

    L70

--- src/main/java/leetcode/top100/L70.java
+++ src/main/java/leetcode/top100/L70.java
@@ -5,4 +5,19 @@ package leetcode.top100;
  * @Date 2022/8/14
  **/
 public class L70 {
+    public static int climbStairs(int n) {
+        if (n<3)
+            return n;
+        int pre1 =1,pre2=2;
+        for(int i=3;i<=n;i++){
+            int cur = pre2+pre1;
+            pre1=pre2;
+            pre2=cur;
+        }
+        return pre2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(3));
+    }
 }
