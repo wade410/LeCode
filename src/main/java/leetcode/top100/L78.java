c5e2bd2f3b064ebe3e0a029bd9f993ada7caae50
呼呼北风 <2535357585@qq.com>
2022-08-23 00:50:02 +0800

    code formate

--- src/main/java/leetcode/top100/L78.java
+++ src/main/java/leetcode/top100/L78.java
@@ -1,5 +1,7 @@
 package leetcode.top100;
 
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,21 +12,36 @@ import java.util.List;
 public class L78 {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
-        if (nums==null || nums.length==0){
+        if (nums == null || nums.length == 0) {
             return list;
         }
         List<Integer> subList = new ArrayList<>();
-        backTracking(nums,list,subList,0);
+        backTracking(nums, list, subList, 0);
         return list;
     }
-    public void backTracking(int[] nums, List<List<Integer>> list, List<Integer> subList, int start){
+
+    public void backTracking(int[] nums, List<List<Integer>> list, List<Integer> subList, int start) {
         list.add(new ArrayList<>(subList));
         for (int i = start; i < nums.length; i++) {
             subList.add(nums[i]);
-            backTracking(nums, list, subList, i+1);
-            subList.remove(subList.size()-1);
+            backTracking(nums, list, subList, i + 1);
+            subList.remove(subList.size() - 1);
         }
 
+    }
+
+    public static void backTracking(int[] nums, List<Integer> list, int start) {
+        System.out.println(list+"--------------"+start);
+
+        for (int i = start; i < nums.length; i++) {
+            list.add(nums[i]);
+            backTracking(nums, list, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        backTracking(new int[] {1, 2, 3, 4}, new ArrayList<>(), 0);
     }
 
 }
