9f0b7ab43b2d05f1aa257115aaf07bf4bf7273b1
呼呼北风 <2535357585@qq.com>
2022-08-24 23:48:24 +0800

    L79

--- src/main/java/leetcode/top100/L79.java
+++ src/main/java/leetcode/top100/L79.java
@@ -5,7 +5,38 @@ package leetcode.top100;
  * @Date 2022/8/24
  **/
 public class L79 {
-    public boolean exist(char[][] board, String word) {
+    private static int[][] direction={{0,1},{0,-1},{1,0},{-1,0}};
+    private static int m,n;
+    public static boolean exist(char[][] board, String word) {
+        if (board==null||word==null||board.length==0||word.length()==0)
+            return false;
+        m=board.length;
+        n=board[0].length;
+        boolean[][] hasVisited = new boolean[m][n];
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (backtracking( r, c,0, board, word,hasVisited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private static boolean backtracking(int i, int j, int k, char[][] board, String word,boolean[][] hasVisited){
+        if (k==word.length())
+            return true;
+        if (i<0||i>=m||j<0||j>=n||board[i][j]!=word.charAt(k)||hasVisited[i][j])
+            return false;
+        hasVisited[i][j]=true;
+        for (int[] d:direction){
+            int nextR=d[0]+i;
+            int nextL=d[1]+j;
+            if (backtracking(nextR,nextL,1+k,board,word,hasVisited)){
+                return true;
+            }
 
+        }
+        hasVisited[i][j]=false;
+        return false;
     }
 }
