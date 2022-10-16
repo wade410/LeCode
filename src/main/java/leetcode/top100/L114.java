5a56c1a1b3a9c16e2f0b721b2af0cdc180196c67
呼呼北风 <2535357585@qq.com>
2022-10-16 22:59:38 +0800

    L114

--- src/main/java/leetcode/top100/L114.java
+++ src/main/java/leetcode/top100/L114.java
@@ -5,4 +5,20 @@ package leetcode.top100;
  * @Date 2022/10/15
  **/
 public class L114 {
+    public void flatten(TreeNode root) {
+        if (root!=null){
+            flatten(root.left);
+            flatten(root.right);
+            TreeNode pre = root.right;
+            root.right=root.left;
+            root.left=null;
+            while (root.right!=null){
+                root=root.right;
+            }
+            root.right=pre;
+        }else {
+            return;
+        }
+
+    }
 }
