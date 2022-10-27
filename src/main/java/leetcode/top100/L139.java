163a97c1340cd7101d5140c7dd8a84c3285f8353
呼呼北风 <2535357585@qq.com>
2022-10-27 23:43:59 +0800

    test

---
+++ src/main/java/leetcode/top100/L139.java
@@ -0,0 +1,14 @@
+package leetcode.top100;
+
+import java.util.List;
+
+/**
+ * @Author HuShengHenGao
+ * @Date 2022/10/27
+ **/
+public class L139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+    }
+
+}
