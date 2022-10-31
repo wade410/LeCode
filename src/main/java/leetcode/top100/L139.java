d8812ba30d2203196aef1e056a3fd5b36cc706ed
呼呼北风 <2535357585@qq.com>
2022-10-31 23:42:52 +0800

    L139

--- src/main/java/leetcode/top100/L139.java
+++ src/main/java/leetcode/top100/L139.java
@@ -1,5 +1,7 @@
 package leetcode.top100;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -7,8 +9,22 @@ import java.util.List;
  * @Date 2022/10/27
  **/
 public class L139 {
-    public boolean wordBreak(String s, List<String> wordDict) {
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+        for (int i = 0; i <= n; i++) {
+            for (String word : wordDict) {
+                if (i > word.length() && word.equals(s.substring(i - word.length(), i))) {
+                    dp[i] = dp[i] || dp[i - word.length()];
+                }
+            }
+        }
+        return dp[n];
+    }
 
+    public static void main(String[] args) {
+        System.out.println(wordBreak("leetcode",new ArrayList<>(Arrays.asList("leet","code"))));
     }
 
 }
